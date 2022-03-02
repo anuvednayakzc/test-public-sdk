fc0020c5787d7f3b032027b877f297ef586f7816
Anuved Nayak <anuved.nayak@zoomcar.com>
2022-03-02 13:36:10 +0530

    - added xcframework

---
+++ ZoomcarSDK.xcframework/ios-x86_64-simulator/ZoomcarSDK.framework/Headers/ZoomcarSDK.h
@@ -0,0 +1,19 @@
+//
+//  ZoomcarSDK.h
+//  ZoomcarSDK
+//
+//  Created by Dhiren Thirani on 05/06/20.
+//  Copyright © 2020 Zoomcar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZoomcarSDK.
+FOUNDATION_EXPORT double ZoomcarSDKVersionNumber;
+
+//! Project version string for ZoomcarSDK.
+FOUNDATION_EXPORT const unsigned char ZoomcarSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomcarSDK/PublicHeader.h>
+
+
